6bcb9ef5cea57e2b42cd7f68c0ce321e59da7eb0
phy6boy <muhsinibnalazeez@gmail.com>
2023-11-17 13:05:25 +0530

    Add Bubble Sort :: C

--- basic/02_sorting/01_bubble_sort/BubbleSort.c
+++ basic/02_sorting/01_bubble_sort/BubbleSort.c
@@ -1,6 +1,63 @@
+/**********************************************************************
+ * Simple bubble sort in C
+ * Author : Muhammed Muhsin A. <muhsinibnalazeez@gmail.com>
+ *
+ * Compile (assuming gcc) : gcc -Wall BubbleSort.c -o BubbleSort.x
+ * Run : ./BubbleSort.x
+ **********************************************************************/
+
 #include <stdio.h>
+
+enum Boolean {
+  FALSE,
+  TRUE
+};
+
+void swap(int *a, int *b) {
+  int tmp;
+  tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+void bubble_sort(int arr[], int n) {
+  /*
+    * Function bubble_sort
+    * sort the arr of size n using Bubble Sort 
+    * the arr is updated inplace
+    */
+  int i, j;
+  enum Boolean swapped = FALSE;
+  for(i=0; i<n-1; i++) {
+    for(j=0; j<n-i-1; j++) {
+      if(arr[j] > arr[j+1]){
+        swap(arr+j, arr+j+1);
+        swapped = TRUE;
+      }
+    }
+    if(swapped == FALSE) break;
+  }
+}
+
+void print_arr(int arr[], int n) {
+  printf("[ ");
+  for(int i=0; i<n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("]\n");
+}
 
 int main(int argc, char *argv[])
 {
+  int arr[] = {45, 3, 23, 11, 89, 13, 133, 76, 15, 10, 9, 47};
+  int n     = sizeof(arr)/sizeof(arr[0]);
+  printf("Before sorting : \n");
+  print_arr(arr, n);
 
+  bubble_sort(arr, n);
+
+  printf("\nAfter sorting : \n");
+  print_arr(arr, n);
+
+  return 0;
 }
